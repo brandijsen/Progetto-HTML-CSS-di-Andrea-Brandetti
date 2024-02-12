9da2b26c4af8c83318f863fcd4afc35c1c238e3e
unknown <andrebrandet93@gmail.com>
2024-02-12 17:54:47 +0100

    hamburger menu: done

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+/* Desktop Nav */
+
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if (top >= offset && top < offset + height ) {
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+
+/* Hamburger Nav */
+
+function toggleMenu() {
+    const menu= document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
 /* About: Show Content*/
 
 let tablinks = document.getElementsByClassName('tab-links');
@@ -11,7 +42,10 @@ function opentab(tabname){
         tabcontent.classList.remove("active-tab")
     }
     event.currentTarget.classList.add("active-link")
+
+    setTimeout(function() {
     document.getElementById(tabname).classList.add("active-tab")
+    }, 300)
 }
 
 /* Contact: EmailJs Service */
